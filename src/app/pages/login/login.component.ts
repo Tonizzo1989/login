15016a3a1aba530003933cc1e32eab8c5226a73a
Antonino Rizzo <toninorizzo1989@yahoo.it.rw>
2024-07-11 14:35:38 +0200

    creato metodo per il localStorge 'onRegister'

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,13 +9,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
   isLoginView: boolean = true;
-
 
   userRegisterObject: any = {
     userName: '',
     password: '',
     emailId: '',
   };
+
+  onRegister() {
+    debugger;
+    const isLocalData = localStorage.getItem('angular18Local');
+
+    if (isLocalData != null) {
+      const localArray = JSON.parse(isLocalData);
+      localArray.push(this.userRegisterObject);
+      localStorage.setItem('angular18Local', JSON.stringify(localArray));
+    } else {
+      const localArray = [];
+      localArray.push(this.userRegisterObject);
+      localStorage.setItem('angular18Local', JSON.stringify(localArray));
+    }
+  }
 }
